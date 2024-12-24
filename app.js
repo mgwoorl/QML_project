7f40d047803307919a334fd08f57b715cf5ab9dd
Екатерина <150834453+lemongoose228@users.noreply.github.com>
2024-12-25 01:25:18 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+function toggleMenu() {
+    const menu = document.getElementById('main-menu');
+    menu.classList.toggle('hidden');
+}
+
+function showPage(page) {
+    const pages = document.querySelectorAll('.page');
+    pages.forEach(p => p.classList.add('hidden')); // Скрыть все страницы
+    document.getElementById(page).classList.remove('hidden'); // Показать выбранную страницу
+}
