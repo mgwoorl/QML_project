591130ab41685e2f95f40ea1fb71d1655a1509ad
Екатерина <150834453+lemongoose228@users.noreply.github.com>
2024-12-25 10:54:46 +0800

    Update app.js

--- app.js
+++ app.js
@@ -8,3 +8,18 @@ function showPage(page) {
     pages.forEach(p => p.classList.add('hidden')); // Скрыть все страницы
     document.getElementById(page).classList.remove('hidden'); // Показать выбранную страницу
 }
+
+//Регистрация
+
+const form = document.getElementById('registrationForm');
+const modal = document.getElementById('successModal');
+const closeModal = document.getElementById('closeModal');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault(); // Предотвращаем отправку формы
+    modal.classList.add('active');
+});
+
+closeModal.addEventListener('click', () => {
+    modal.classList.remove('active');
+});
